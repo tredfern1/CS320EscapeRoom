422ee2d883c7ad3601de5e5f38327a161fe1d207
Kyle Smith <ksmith126@ycp.edu>
2021-03-05 13:27:59 -0500

    refactoring/ altered jsp

---
+++ CS320_EscapeRoom/src/edu/ycp/cs320/CS320_EscapeRoom/servlet/GameServlet.java
@@ -0,0 +1,32 @@
+package edu.ycp.cs320.CS320_EscapeRoom.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ycp.cs320.CS320_EscapeRoom.controller.AddNumbersController;
+import edu.ycp.cs320.CS320_EscapeRoom.model.Numbers;
+
+public class GameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		System.out.println("AddNumbers Servlet: doGet");	
+		
+		// call JSP to generate empty form
+		req.getRequestDispatcher("/_view/game.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		
+	}
+}
---
+++ CS320_EscapeRoom/war/_view/game.jsp
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+
+<html>
+	<head>
+		<title>Game</title>
+		<style type="text/css">
+		.error {
+			color: red;
+		}
+		
+		td.label {
+			text-align: right;
+		}
+		</style>
+	</head>
+
+	<body>
+		        
+            
+        <div >
+            <h1>Escape Room</h1>
+         </div>
+        
+            <div >
+                <p> you are in a dark room</p>
+            </div>
+            
+          
+            
+  
+	</body>
+</html>
