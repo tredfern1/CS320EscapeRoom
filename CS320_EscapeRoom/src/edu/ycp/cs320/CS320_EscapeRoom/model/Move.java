87720d2d7c0499f6229f1d3ad843ff86f0a03a7b
Kyle Smith <ksmith126@ycp.edu>
2021-03-20 11:57:03 -0400

    move methods

--- CS320_EscapeRoom/src/edu/ycp/cs320/CS320_EscapeRoom/model/Move.java
+++ CS320_EscapeRoom/src/edu/ycp/cs320/CS320_EscapeRoom/model/Move.java
@@ -2,6 +2,22 @@ package edu.ycp.cs320.CS320_EscapeRoom.model;
 
 public class Move {
 
+	//splits the move into two separate strings in an array
+	public String[] split(String move) {
+		return move.split(" ");
+	}
+	
+	//if the command consists of two words it is considered valid
+	//if not then the move is not valid
+	public boolean validate(String[] move) {
+		if(move.length == 2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
 	
 	
 }
