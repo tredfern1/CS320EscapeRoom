4e37558da5ce8b8f7d0c1a4e5ca2cb7f4f95268e
tredfern1 <tredfern@ycp.edu>
2021-03-21 13:43:45 -0400

    More work on Milestone 1
    - Room class changed to Spots
    - Created Map and MapController Classes

---
+++ CS320_EscapeRoom/src/edu/ycp/cs320/CS320_EscapeRoom/controller/mapController.java
@@ -0,0 +1,12 @@
+package edu.ycp.cs320.CS320_EscapeRoom.controller;
+
+import edu.ycp.cs320.CS320_EscapeRoom.model.Map;
+import edu.ycp.cs320.CS320_EscapeRoom.model.Spot;
+
+
+public class mapController {
+	
+	Map m;
+	
+	
+}
