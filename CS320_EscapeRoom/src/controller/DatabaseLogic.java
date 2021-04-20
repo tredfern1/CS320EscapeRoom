da4d2fa62052ac8b8f42603bd0ab74d100a0dd70
Kyle Smith <ksmith126@ycp.edu>
2021-04-20 16:20:29 -0400

    all branches merged successfully

--- CS320_EscapeRoom/src/controller/DatabaseLogic.java
+++ CS320_EscapeRoom/src/controller/DatabaseLogic.java
@@ -22,7 +22,7 @@ public class DatabaseLogic {
 	public int getRoom() {
 		
 		// get the list of (Author, Book) pairs from DB
-		int room = db.getRoom();
+		int room = db.getRoom(); 
 
 		System.out.println("The call from the class is: " + room);
 		// return authors for this title
@@ -84,6 +84,11 @@ public class DatabaseLogic {
 		return result;
 	}
 
+	public List<String> addLog(String line){
+		List<String> result = new ArrayList<String>();
+		result = db.addLog(line);
+		return result;
+	}
 	
 	public void removeFromMapInventory(String item) {
 		db.removeFromMapInventory(item);
