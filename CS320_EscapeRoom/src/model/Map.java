7b5f1612e4148428a1e865938f6d8463544c3148
tredfern1 <tredfern@ycp.edu>
2021-04-27 18:06:42 -0400

    Hints + book
    - added a hint system to the game
    - make the 'book' item readable

--- CS320_EscapeRoom/src/model/Map.java
+++ CS320_EscapeRoom/src/model/Map.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Map {
 	
 	Spot[][] spots;
+	String[][] hints;
 
-	public Map(int room, List<String> newMapInventory) {
+	public Map(int room, List<String> newMapInventory) {		
 		// Initialize Map Spots
 		spots = new Spot[3][3]; 
 		for(int i = 0; i < 3; i++)
@@ -17,6 +18,14 @@ public class Map {
 				spots[i][j] = new Spot();
 			}
 		}
+		//initializes the hints (with no hint for empty spots)
+		hints = new String[3][3];
+		for (int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++) {
+				hints[i][j] = "There is nothing here";
+			}
+		}
+		
 		//Set descriptions
 		if(room == 1)
 		{
@@ -38,6 +47,12 @@ public class Map {
 			spots[1][2].setdescription("There's a red door with a keyhole, it's been unlocked!");
 			//Description setters after actions #2
 			spots[0][2].setdescription("The crate is broken and lying on the ground. This is where you found the key.");
+			
+			//hints for room 1
+			hints[1][1] = "Move around with the command: move north/south/east/west";
+			hints[0][1] = "Pick up items with the command: take 'item";
+			hints[0][2] = "Maybe there is something in this room that could help you break the crate?";
+			hints[2][2] = "They look friendly! Maybe you should take one with you?";
 		}
 		if(room == 2)
 		{	//set first descriptions/items of the room
@@ -65,31 +80,38 @@ public class Map {
 			spots[0][2].setdescription("The safe is unlocked");
 			
 			spots[0][1].setdescription("There's the hole where the rat brought you gold from");
+			
+			//hints for room 2
+			hints[0][2] = "The code is probably hidden somewhere in this room. Or you could get a lucky guess?";
 		}
 		if (room == 3) {
 			//starting descriptions
-			spots[0][0].setdescription("The southwestern corner of the third room. There is a bookshelf here, but nothing stands out at the moment.");
-			spots[0][1].setdescription("The western side of the third room. There is nothing here");
-			spots[0][2].setdescription("The northwestern corner of the third room. There is nothing here");
+			spots[0][0].setdescription("You are in the southwestern corner of the third room. There is a bookshelf here, but nothing stands out at the moment.");
+			spots[0][1].setdescription("You are on the western side of the third room. There is nothing here");
+			spots[0][2].setdescription("You are in the northwestern corner of the third room. There is nothing here");
 			
 			spots[1][0].setdescription("You entered room 3. You are on the southern side of the room. You can south to return to room 2.");
-			spots[1][1].setdescription("The center of the third room. There is a grand piano here. On the sheet music stand there appears to be a multiple choice question. It reads: "
+			spots[1][1].setdescription("You are in the center of the third room. There is a grand piano here. On the sheet music stand there appears to be a multiple choice question. It reads: "
 					+ 					"<br> What is Isaac Asimov's first Law of Robotics? "
 					+ 					"<br> (A) 'A robot must obey orders given it by human beings except where such orders would conflict with the First Law."  
 					+					"<br> (B) 'A robot may not harm humanity, or, by inaction, allow humanity to come to harm.' "
 					+ 					"<br> (C) 'A robot may not injure a human being or, through inaction, allow a human being to come to harm.' "
 					+ 					"<br> (D) 'A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.' ");
-			spots[1][2].setdescription("The northern side of the third room. There is a locked door here.");
+			spots[1][2].setdescription("You are on the northern side of the third room. There is a locked door here.");
 			
-			spots[2][0].setdescription("The southeastern corner of the third room. There is nothing here");
-			spots[2][1].setdescription("The eastern side of the third room. There is nothing here");
-			spots[2][2].setdescription("The northeastern side of the third room. There is nothing here");
+			spots[2][0].setdescription("You are in the southeastern corner of the third room. There is nothing here");
+			spots[2][1].setdescription("You are on the eastern side of the third room. There is nothing here");
+			spots[2][2].setdescription("You are in the northeastern side of the third room. There is nothing here");
 			
 			//descriptions after changes are made
-			spots[0][0].setdescription("The southwestern corner of the third room. One book stands out to you; 'I, Robot' by Isaac Asimov");
-			spots[0][0].setdescription("The southwestern corner of the third room. There is a bookshelf here. The rest of the books don't seem helpful, though.");
+			spots[0][0].setdescription("You are in the southwestern corner of the third room. One book stands out to you; 'I, Robot' by Isaac Asimov");
+			spots[0][0].setdescription("You are in the southwestern corner of the third room. There is a bookshelf here. The rest of the books don't seem helpful, though.");
 			
-			spots[1][1].setdescription("The center of the third room. There is a grand piano here. You have already solved this puzzle. It appears the last door has unlocked, also.");
+			spots[1][1].setdescription("You are in the center of the third room. There is a grand piano here. You have already solved this puzzle. It appears the last door has unlocked, also.");
+			
+			//hints for room 3
+			hints[1][1] = "The answer is likely hidden somewhere in this room. And don't Google it, that's cheating!";
+			hints[0][0] = "Come back when you figure out what book you need";
 		}
 		
 		//SET THE ITEMS IN THE MAP BASED ON THE ARRAY OF MAP ITEMS
@@ -119,6 +141,9 @@ public class Map {
 		this.spots = s;
 	}	
 	
+	public String getHint(int x, int y) {
+		return hints[x][y];
+	}
 	
 	public Spot getSpot(int x, int y) {
 		return spots[x][y];
