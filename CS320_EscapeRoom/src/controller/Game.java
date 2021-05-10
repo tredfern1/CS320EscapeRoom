541ff7b204e556eb5dd300d1853a6e3789db53ab
Derek Herr <dherr@ycp.edu>
2021-05-10 18:50:05 -0400

    bug

--- CS320_EscapeRoom/src/controller/Game.java
+++ CS320_EscapeRoom/src/controller/Game.java
@@ -378,7 +378,7 @@ public class Game {
 			}
 			else if(command[0].contains("pickup"))
 			{
-				return "You can't pickup anything";
+				return "You can't pickup that";
 			}
 			else if(command[0].contains("use"))
 			{
